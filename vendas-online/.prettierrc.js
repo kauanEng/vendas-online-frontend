28e55d97141d2fc1626e710b2bf1a9266fdccf8c
kauanEng <kauansantos83@gmail.com>
2023-06-07 18:37:16 -0300

    feat: Adicionado as configurações básicas do projeto Prettier e eslint

---
+++ vendas-online/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true,  //ELE ADICIONA SEMPRE "," EM TODO FINAL DE CARACTERE OU FUNÇÃO ETC. 
+    trailingComma: 'all', //SEMPRE TENHA A VIRGULA NO FINAL.
+    singleQuote: true, // COLOCAR SEMPRE ASPAS SIMPLES
+    printWidth: 100, // ELE QUEBRA 100 CARACTERES LINHA
+    tabWidth: 2, // DOIS ESPAÇOS
+};
